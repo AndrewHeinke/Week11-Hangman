8be709b4af17af10607e83282e5999eef17702f3
Andrew <andrew.j.heinke@gmail.com>
2016-07-05 21:22:24 -0500

    added package json file main js updates

--- main.js
+++ main.js
@@ -1,2 +1,17 @@
 // game is played and contains all of the user-input
 var game = require('./game.js');
+var inquirer = require('inquirer');
+
+inquirer.prompt([
+  {
+    name: 'letter',
+    type: 'text',
+    message: 'Guess a Letter: ',
+    validate: function(str) {
+      var regEx = new RegExp("[a-zA-Z]");
+      return regEx.test(str);
+    }
+  }
+]).then(function(answers) {
+    var letter = answers.letter.toLowerCase();
+});
