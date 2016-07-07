a607f82ca44823b5854348b77a2c4771a584d59a
Andrew <andrew.j.heinke@gmail.com>
2016-07-06 19:43:50 -0500

    added basic boilerplate

--- letter.js
+++ letter.js
@@ -1,1 +1,12 @@
 // control whether or not a letter appears as a "_" or as itself on-screen
+var game = require('./game.js');
+
+var displayWord = function() {
+  var targetWord = game.wordBank[Math.floor(Math.random() * wordBank.length)].toLowerCase();
+  var chosenWord = [];
+    for (var i = 0; i < targetWord.length; i++) {
+      chosenWord[i] = "_ ";
+    }
+};
+
+displayWord();
--- word.js
+++ word.js
@@ -1,1 +1,2 @@
 // contain all of the methods which will check the letters guessed versus the random word selected
+var letter = require('./letter.js');
